d0a5aecf733c104042dcf1ae1517efa08414f859
Rangel Reale <rangelspam@gmail.com>
2024-06-19 06:51:40 -0300

    docs

--- copyfile.go
+++ copyfile.go
@@ -2,6 +2,7 @@ package copyfile
 
 import "github.com/rrgmc/debefix"
 
+// New creates an instance of the CopyFile debefix plugin.
 func New(options ...Option) *CopyFile {
 	ret := &CopyFile{}
 	for _, opt := range options {
@@ -10,10 +11,10 @@ func New(options ...Option) *CopyFile {
 	return ret
 }
 
+// NewOptions returns debefix [debefix.LoadOption] and [debefix.ResolveOption] to use the plugin.
 func NewOptions(options ...Option) (*CopyFile, []debefix.LoadOption, []debefix.ResolveOption) {
 	c := New(options...)
 	return c,
 		[]debefix.LoadOption{debefix.WithLoadValueParser(c)},
 		[]debefix.ResolveOption{debefix.WithRowResolvedCallback(c)}
 }
-
